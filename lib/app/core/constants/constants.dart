500f8785d4858ead4a5455fe583c10cc1a119ca6
= <=>
2024-12-20 07:33:54 -0300

    .

--- lib/app/core/constants/constants.dart
+++ lib/app/core/constants/constants.dart
@@ -4,8 +4,8 @@ class Constants {
   Constants._();
 
   static const String USER_ID = 'vgl7074qqf6s2tq1ri55hbooy';
-  static const String CLIENT_ID = '';
-  static const String CLIENT_SECRET = '';
+  static const String CLIENT_ID = 'b00c1dfce3a6495c9bd2632e0907be67';
+  static const String CLIENT_SECRET = '2caf2b64f5c347549e1449df308741cf';
 
 
 }
